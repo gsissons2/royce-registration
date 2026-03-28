84716cd3faeccbb2ef22b7829456438cdd17d7d5
Graham Sissons <studio@prycedigital.com>
2026-03-28 18:35:15 +1100

    fix: temporarily disable PWA service worker to debug API issue

--- next.config.js
+++ next.config.js
@@ -1,27 +1,6 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/.*\/api\/.*/i,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'api-cache',
-        expiration: {
-          maxEntries: 32,
-          maxAgeSeconds: 60,
-        },
-        networkTimeoutSeconds: 10,
-      },
-    },
-  ],
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = withPWA(nextConfig)
+module.exports = nextConfig
